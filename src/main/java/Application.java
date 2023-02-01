bdaf728c9d4d37329090bdf33facf5ca038e97a7
tedbeast <tedbalashov@gmail.com>
2023-02-01 12:48:41 -0500

    main update

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,17 +9,17 @@ public class Application {
      */
     public static void main(String[] args) {
 
-        System.out.println("This bird is 2 months old, and should Tweet.");
+        System.out.println("This bird is 2 months old, and should Tweet. Here's the current sound: ");
         Bird b1 = new Bird();
         b1.ageMonths = 2;
         System.out.println(b1.sound());
 
-        System.out.println("This bird is 6 months old, and should Chirp.");
+        System.out.println("This bird is 6 months old, and should Chirp. Here's the current sound: ");
         Bird b2 = new Bird();
         b2.ageMonths = 6;
         System.out.println(b2.sound());
 
-        System.out.println("This bird is 3 months old, and should Chirp.");
+        System.out.println("This bird is 3 months old, and should Chirp. Here's the current sound: ");
         Bird b3 = new Bird();
         b3.ageMonths = 3;
         System.out.println(b3.sound());
